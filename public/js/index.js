ec0d1d7ca68cff339c05c72b6fa35477b01c8a27
bill-hennessey <bhennessey1020@gmail.com>
2021-09-07 16:42:43 -0600

    tweaking event handlers

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,11 @@
+const router = require("express").Router();
+const express = require("express");
+const mysql = require("mysql2");
+
 const startBtn = document.getElementById("TBD");
 
 // function to GET upcoming events for homepage
+// UPDATE ROUTES
 const dispUpcomingEvents = async () =>
   await fetch("/api/users/login", {
     method: "GET",
@@ -9,10 +14,10 @@ const dispUpcomingEvents = async () =>
 //   for login event handler
 const loginFormHandler = async (event) => {
   event.preventDefault();
-
+  // UPDATE IDS
   const email = document.querySelector("#email-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
-
+  // UPDATE ROUTES
   if (email && password) {
     const response = await fetch("/api/users/login", {
       method: "POST",
@@ -32,11 +37,12 @@ const loginFormHandler = async (event) => {
 // for signup event handler
 const signupFormHandler = async (event) => {
   event.preventDefault();
-
+  // UPDATE IDS
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
   if (email && password) {
+    // UPDATE ROUTES
     const response = await fetch("/api/users", {
       method: "POST",
       body: JSON.stringify({ email, password }),
@@ -52,7 +58,32 @@ const signupFormHandler = async (event) => {
   }
 };
 
+// AFTER LOGIN
+const searchInterestHandler = async (event) => {
+  event.preventDefault();
+  // UPDATE IDS
+  const searchTerm = document.querySelector("#search-interest").value.trim();
+
+  if (searchTerm) {
+    // UPDATE ROUTES
+    const response = await fetch("/api/users", {
+      method: "GET",
+      body: JSON.stringify({ searchTerm }),
+    });
+
+    // where is response.ok set?
+    if (response.ok) {
+      // where does this go?
+      document.location.replace("/");
+    } else {
+      alert("Please try again.");
+    }
+  }
+};
+
 // event handlers
+// UPDATE IDS
+// ARE WE USING THE FORM? OR THE BUTTON?
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);

97ec4a5ef5f1cd83b1b0e46d9088d54a72ff6fce
bcbridges <bcb35b@gmail.com>
2021-09-08 14:38:18 -0600

    Un-commented server.js file that requires routing, and added multi-direcitonal relationships to models.

--- models/index.js
+++ models/index.js
@@ -4,14 +4,16 @@ const EventTags = require("./EventTags");
 const Events = require("./Events");
 
 User.hasMany(Events, { foreignKey: "owner_id" });
+Events.belongsTo(User, { foreignKey: "owner_id" });
 
 User.hasMany(EventAttend, { foreignKey: "attendee" });
+EventAttend.belongsTo(User, { foreignKey: "attendee" });
 
 Events.hasMany(EventAttend, { foreignKey: "event_id" });
+EventAttend.belongsTo(Events, { foreignKey: "event_id" });
 
-EventTags.hasMany(Events, {
-  foreignKey: "tag_id",
-});
+EventTags.hasMany(Events, { foreignKey: "tag_id" });
+Events.belongsTo(EventTags, { foreignKey: "tag_id" });
 
 module.exports = {
   User,
